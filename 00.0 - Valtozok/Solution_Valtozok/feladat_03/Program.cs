b7758298f8dedb9e5c5086ec5568cd9c9fd8647c
MoreHelload <milan.kondor06@gmail.com>
2023-09-08 12:01:54 +0200

    feladat_03&04

---
+++ 00.0 - Valtozok/Solution_Valtozok/feladat_03/Program.cs
@@ -0,0 +1,7 @@
+﻿int constructionStartYear = 1368;
+int constructionEndYear = 1644;
+int wallLength = 7200;
+string formerName = "Ming";
+string presentName = "Kínai Nagyfal";
+
+Console.WriteLine("A {0} dinasztia {1} és {2} közt építette a ma látható {3} {4}km hosszú szakaszát.", formerName, constructionStartYear, constructionEndYear, presentName, wallLength);
---
+++ 00.0 - Valtozok/Solution_Valtozok/feladat_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Oroszország legmodernebb vadászrepülőgépe a *2008*-ban bemutatott *Sukhoj* gyártotta *SU-35S* amely képes a
+// *2.4* Mach sebességre, ami 720*m/s*-nak felel meg.
+
+int productionYear = 2008;
+string companysName = "Sukhoj";
+string modelsName = "SU-35S";
+double machSpeed = 2.4;
+string msUnitMeasurement = "m/s";
+
+Console.WriteLine("Oroszország legmodernebb vadászrepülőgépe a {0}-ban bemutatott {1} gyártotta {2}, amely képes a {3} Mach sebességre, ami 720{4}-nak felel meg.", productionYear, companysName, modelsName, machSpeed, msUnitMeasurement);
