5cdbc1d6b04bd2ff83cf546095bb35a9ef98b982
KonMil <milan.kondor06@gmail.com>
2023-09-08 09:36:33 +0200

    feladat_02

---
+++ 00.0 - Valtozok/Solution_Valtozok/feladat_02/Program.cs
@@ -0,0 +1,5 @@
+﻿string cpuSeries = "i7";
+int cpuType = 8700;
+double cpuSpeed = 4.6;
+
+Console.WriteLine("Az Intel {0} sorozatjelzésű mikroprocesszor {1} típusának munkafrekvenciája {2}", cpuSeries, cpuType, cpuSpeed);
