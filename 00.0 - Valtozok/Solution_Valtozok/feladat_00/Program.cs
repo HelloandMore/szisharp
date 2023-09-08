d878532c8e16ac2ac3ba70e36c0757e2ef7dcbe8
KonMil <milan.kondor06@gmail.com>
2023-09-08 08:34:06 +0200

    second_commit
    igen

---
+++ 00.0 - Valtozok/Solution_Valtozok/feladat_00/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Katarzyna Skowronksa-Dolata";
+string nationality = "Lengyel";
+int yearOfBirth = 1983;
+string birthPlace = "Warszava";
+double height = 1.89;
+int weight = 75;
+int hitHeight = 317;
+int rampartHeight = 302;
+int playerNumber = 2;
+bool isCaptain = true;
+char captainLetter = 'c';
+
+Console.ReadKey();
