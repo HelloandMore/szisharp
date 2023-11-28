5fdc5969d0fcd8d6a527be795936a8870784738d
konmil <milan.kondor06@gmail.com>
2023-11-28 08:10:23 +0100

    munkas_fizetes + valamixd 2023. 11. 28.

---
+++ 11 - Fuggvenyek/Fuggvenyek_Solution/Feladat_06/Program.cs
@@ -0,0 +1,44 @@
+﻿/* A program bekér egy dolgozat pontszámot (x) a felhasználótól és kiír egy
+érdemjegyet az alábbiak szerint! : 
+1: x<50; 2: 50<=x<60; 3: 60<=x<70; 4: 70<=x<85; 5: x>=85. */
+
+using IOLibrary;
+
+double point;
+
+point = ExtendedConsole.ReadDouble("Add meg a dolgozatban elért pontszámod > ");
+
+double gradeCalculator(double point)
+{
+	int returningValue;
+
+	double percent = point;
+
+	if (percent < 50)
+	{
+		returningValue = 1;
+	}
+	else if (percent >= 50 && percent < 60)
+	{
+		returningValue = 2;
+	}
+	else if (percent >= 60 && percent < 70)
+	{
+		returningValue = 3;
+	}
+	else if (percent >= 70 && percent < 85)
+	{
+		returningValue = 4;
+	}
+	else if (percent >= 85)
+	{
+		returningValue = 5;
+	}
+	else
+	{
+		returningValue = 0;
+	}
+	return returningValue;
+}
+
+Console.WriteLine($"A dolgozatra kapott jegyed a következő: {gradeCalculator(point)}");
---
+++ 11 - Fuggvenyek/Fuggvenyek_Solution/Feladat_11/Program.cs
@@ -0,0 +1,17 @@
+﻿using IOLibrary;
+
+const int BASE_WORKING_HOURS = 40;
+
+string nameOfWorker = ExtendedConsole.ReadString("Add meg a dolgozó nevét > ");
+double workersWorkingHours = ExtendedConsole.ReadDouble("Add meg mennyit dolgozott > ");
+
+double regularPayment = CalculatePayment(BASE_WORKING_HOURS);
+double overtimePayment = workersWorkingHours > BASE_WORKING_HOURS ? CalculatePayment(workersWorkingHours - BASE_WORKING_HOURS, 1500) : 0;
+
+double payment = regularPayment + overtimePayment;
+
+Console.WriteLine($"{nameOfWorker} fizetése: {payment} HUF");
+Console.ReadKey();
+
+
+double CalculatePayment(double workingHours, int wage = 1000) => workingHours * wage;
