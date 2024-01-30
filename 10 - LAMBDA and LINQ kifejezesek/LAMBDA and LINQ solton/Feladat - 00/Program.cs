89a5b1bc4c87d14509edcc7572805568c8a72ac3
konmil <milan.kondor06@gmail.com>
2024-01-30 09:41:07 +0100

    2024. 01. 30. - Linq struggle

---
+++ 10 - LAMBDA and LINQ kifejezesek/LAMBDA and LINQ solton/Feladat - 00/Program.cs
@@ -0,0 +1,41 @@
+﻿List<Student> students = new List<Student>
+{
+	new Student("Hétfő Henrik", 10, 154),
+	new Student("Kedd Kinga", 14, 250),
+	new Student("Szerda Szilárd", 9, 98),
+	new Student("Csütörtök Csongor", 11, 198),
+	new Student("Péntek Petra", 13, 245),
+	new Student("Szombat Szimonetta", 10, 152),
+	new Student("Vasárnap Virág", 13, 221),
+};
+
+
+// Mennyi a legnagyobb pontszám?? 
+int maxPoints = students.Max(x => x.Points);
+
+// Ki érte ezt el??
+Student bestStudent = students.MaxBy(x => x.Points);
+
+// Mennyi a legkisebb pontszám??
+int minPoints = students.Min(x => x.Points);
+
+// Ki érte ezt el??
+Student worstStudent = students.MinBy(x => x.Points);
+
+// Mik a diákok nevei?
+List<string> studentNames = students.Select(students => students.Name).ToList();
+
+// Kik szereztek többet 200 pontnál??
+List<string> studentAboveTwohundred = students.Where(x => x.Points > 200).Select(x => x.Name).ToList();
+
+// Diákok neve ABC sorrendben..
+List<string> studentsInAlphabeticOrder = students.OrderBy(x => x.Name).Select(x => x.Name).ToList();
+
+// ..majd pontok alapján csökkenő sorrendbe
+List<string> orderStudentsbyNameandPoints = students.OrderBy(x => x.Name).ThenByDescending(x => x.Points).Select(x => x.Name).ToList();
+
+// Diákok csökkenő sorrendbe rendezése osztályok alapján ..
+// ..majd diákok csökkenő sorrendbe rendezése pontok alapján
+List<string> orderStudentsByGradeandPoints = students.OrderByDescending(x => x.Grade).ThenByDescending(x => x.Points).Select(x => x.Name).ToList();
+
+// Évfolyamonként elért pontszám évfolyam szerint csökkenő sorrendben
---
+++ 10 - LAMBDA and LINQ kifejezesek/LAMBDA and LINQ solton/Feladat - 00/Student.cs
@@ -0,0 +1,13 @@
+﻿public class Student
+{
+	public string Name { get; set; }
+	public int Grade { get; set; }
+	public int Points { get; set; }
+
+	public Student(string name, int grade, int points)
+	{
+		this.Name = name;
+		this.Grade = grade;
+		this.Points = points;
+	}
+}
