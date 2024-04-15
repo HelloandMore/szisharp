3b6e15093a79a16a3ad4232857d201fa0307b540
HelloandMore <milan.kondor06@gmail.com>
2024-04-15 17:03:16 +0200

    2024. 04. 15. - sikerult a hazi noway, es mukodik :ooooo

--- 13 - HTTP/RestApiClients/Custom.Library/SystemExtensions/ExtendedSystem.cs
+++ 13 - HTTP/RestApiClients/Custom.Library/SystemExtensions/ExtendedSystem.cs
@@ -4,7 +4,7 @@ public static class ExtendedSystem
 {
     public static void WriteToConsole(this object objectToDisplay) => Console.Write(objectToDisplay);
 
-    public static void WriteCollectionToConsole<T>(this ICollection<T> collectionToDisplay)
+    public static void WriteCollectionToConsole<T>(this ICollection<T> collectionToDisplay) where T : class
     {
         foreach (object item in collectionToDisplay)
         {
