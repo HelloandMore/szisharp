efbba5ade24762274d2b42d77aaec5b3cbbc20df
Milan <53042337+HelloandMore@users.noreply.github.com>
2025-01-14 13:46:25 +0100

    Működőképes verziója az auto_git.bat-nak

--- upload_manually.cmd
+++ upload_manually.cmd
@@ -1,29 +1,36 @@
 @echo off
-pushd %~0\..
-set /p comment="Add meg a commentet > "
+title Auto Git
+pushd "%~dp0"
 
-@echo on
-git add .
-set adderr=%errorlevel%
-@echo off
-if not %adderr%==0 (goto :error)
+net file 1>nul 2>nul
+if %errorlevel%==0 (
+    goto :begin
+) else (
+    powershell.exe Start-Process '%~0' -verb runas
+    exit /b %errorlevel%
+)
 
-@echo on
-git commit -m "%date% - %comment%"
-set commiterr=%errorlevel%
-@echo off
-if not %commiterr%==0 (goto :error)
+:begin
+set /p selection="1. Push vagy 2. Pull > "
 
-@echo on
+if %selection%==1 (
+    git add .
+    git status
+    goto :push
+) else if %selection%==2 (
+    git pull
+    goto :end
+) else (
+    echo Hibás választás!
+    goto :begin
+)
+
+:push
+set /p commitmessage="Commit message: "
+git commit -m "%date% - %commitmessage%"
 git push
-@echo off
-timeout 5 /nobreak
-exit /b
 
-:error
-echo.
-echo.
+:end
 net helpmsg %errorlevel%
 timeout 5 /nobreak > nul
-pause
-exit /b
+exit /b %errorlevel%githju
